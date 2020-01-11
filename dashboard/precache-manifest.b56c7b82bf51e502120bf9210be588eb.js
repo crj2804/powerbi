c5606780a594502bcabaf72e548269938f25a065
crj2804git commit -m first <crj2804@DESKTOP-G7MD7UE.localdomain>
2020-01-11 13:39:21 -0500

    first

---
+++ dashboard/precache-manifest.b56c7b82bf51e502120bf9210be588eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b6607a7e5b27fd981c8230212670b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5af71833167f6e00252d",
+    "url": "/static/css/main.21aa5e29.chunk.css"
+  },
+  {
+    "revision": "0feff3941798c4271890",
+    "url": "/static/js/2.aa1a0abd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.aa1a0abd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5af71833167f6e00252d",
+    "url": "/static/js/main.5d105a2e.chunk.js"
+  },
+  {
+    "revision": "7cc77c15693fe0624208",
+    "url": "/static/js/runtime-main.86f6c891.js"
+  }
+]);
